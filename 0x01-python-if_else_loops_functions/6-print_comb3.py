f2c501e882ed1a6b0ae78de12b30c88d090e3f12
Remso27 <kouassiremi27@gmail.com>
2022-11-26 17:37:51 +0000

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 for number in range(100):
     if int(number / 10) != number % 10 and int(number / 10) < number % 10:
         print("{}{}".format(int(number / 10), number % 10), end="")
