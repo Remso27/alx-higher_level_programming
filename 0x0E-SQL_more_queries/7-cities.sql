f9df902f9b23fbb724471d7ded3440e56213610e
Remso27 <kouassiremi27@gmail.com>
2022-12-07 10:41:27 -0800

    task7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- Write a script that creates the database hbtn_0d_usa and the table cities
+-- cities description:
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT AUTO_INCREMENT, name VARCHAR(256), FOREIGN KEY(state_id), PRIMARY KEY(id));
