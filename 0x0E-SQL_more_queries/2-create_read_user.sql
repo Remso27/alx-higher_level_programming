04264717d15e0d4b6ff9b20ab1cf431d82677e83
Remso27 <kouassiremi27@gmail.com>
2022-12-07 08:23:54 -0800

    task2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Write a script that creates the database hbtn_0d_2
+-- If the database hbtn_0d_2 already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Write a script that creates the database user_0d_2
+-- If the user user_0d_2 already exists, your script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
