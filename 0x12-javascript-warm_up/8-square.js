e0a859073e6c9ebbb57834678bf7c9d28e79f283
Remso27 <kouassiremi27@gmail.com>
2022-12-21 08:26:07 -0800

    task8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+const { argv } = require('process');
+const size = parseInt(argv[2]);
+
+const printSquare = (size) => {
+  const row = 'X'.repeat(size);
+  for (let i = 0; i < size; i++) console.log(row);
+};
+
+Number.isInteger(size) ? printSquare(size) : console.log('Missing size');
